265b9f9b68f969df620204264673147ef0e0122e
Jonas Claeson <jonas.cl@protonmail.com>
2025-12-08 20:11:42 +0100

    Add Type representation for Once types
    Defines the core type system: type variables, Unit, Void, products (*),
    sums (+), and function types (->). These correspond to categorical
    constructs: terminal/initial objects, products, coproducts, and exponentials.

---
+++ compiler/src/Once/Type.hs
@@ -0,0 +1,19 @@
+module Once.Type
+  ( Type (..)
+  , Name
+  ) where
+
+import Data.Text (Text)
+
+-- | Type variable or constructor name
+type Name = Text
+
+-- | Once type representation
+data Type
+  = TVar Name              -- ^ Type variable: A, B, etc.
+  | TUnit                  -- ^ Unit type (terminal object)
+  | TVoid                  -- ^ Void type (initial object)
+  | TProduct Type Type     -- ^ Product type: A * B
+  | TSum Type Type         -- ^ Sum type: A + B
+  | TArrow Type Type       -- ^ Function type: A -> B
+  deriving (Eq, Show)
